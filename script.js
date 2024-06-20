34e09c2f63efb10eb23c29fdd7754356c6682df4
arturix2 <123446162+arturix2@users.noreply.github.com>
2024-06-20 12:56:26 +0000

    19.06.2024

--- script.js
+++ script.js
@@ -4,7 +4,16 @@ function atjaunotIetvaru(which) {
 
 window.onload = function() {
   zimetuzcanva();
-}
+    var navLinks = document.querySelectorAll('.topnav a');
+    navLinks.forEach(function(link) {
+      link.addEventListener('click', function() {
+        navLinks.forEach(function(link) {
+          link.classList.remove('active');
+        });
+        this.classList.add('active');
+      });
+    });
+  }
 
 function zimetuzcanva() {
   var kanva  = document.getElementById("zimejums");
@@ -55,6 +64,21 @@ function aprekins() {
   let vards = document.getElementById("vards").value;
   let pirmais = parseFloat(document.getElementById("a").value);
   let otrais = parseFloat(document.getElementById("b").value);
+  if (!vards.match(/^\S[a-zA-Zā-žĀ-Ž\s]*$/)) {
+    alert("Lūdzu ievadiet vārdu!");
+    return;
+  }
+  
+  if (pirmais === "" || otrais === "" || isNaN(pirmais) || isNaN(otrais)){
+    alert("Ievadi skaitli!");
+    return;
+  }
+
+  if (!Number.isInteger(Number(pirmais)) || !Number.isInteger(Number(otrais))) {
+    alert("Ievadi veselu skaitli!");
+    return;
+  }
+  
   let summa = pirmais * otrais;
   console.log("Tevi sauc: " + vards + " Atbilde ir:" + summa);
   document.getElementById("rezultats").innerHTML = "Tevi sauc: " + vards + "<br>Tu ievadīji a vērtību: " + pirmais + "<br> un otrā vērtība: "+ otrais +"<br><br> Atbilde ir:" + summa;
